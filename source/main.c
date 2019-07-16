592c84a0a4168c5359205e377285a68302a74a4c
Vlad <shadow.gentoo@gmail.com>
2019-07-16 13:53:07 +0300

    Can receive messages on /dev/ttyACM0 9600 Baud 8 bit no parity

--- source/main.c
+++ source/main.c
@@ -1,8 +1,50 @@
 #include "stdlib.h"
 #include "stdio.h"
+#include "termios.h"
+#include "unistd.h"
+#include "fcntl.h"
+#include "string.h"
 
 int main(int argc, char const *argv[])
 {
-    printf("hello world!");
+    int fd;
+    char buffer[255];
+    memset(&buffer, 0, 255);
+
+    fd = open("/dev/ttyACM0",O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd == -1)
+    {
+        printf("can't open device\n");
+        return 1;
+    }
+
+    struct termios terminal;
+
+    tcgetattr(fd, &terminal);
+
+    cfsetispeed(&terminal, B9600);
+    cfsetospeed(&terminal, B9600);
+    //set it to local, so we will not own port and we will read it
+    terminal.c_cflag |= (CLOCAL | CREAD);
+    //set data to 8 bit
+    terminal.c_cflag &= ~CSIZE; /* Mask the character size bits */
+    terminal.c_cflag |= CS8;    /* Select 8 data bits */
+    tcsetattr(fd, TCSANOW, &terminal);
+    
+    fcntl(fd, F_SETFL, 0);
+
+    printf("ready to receive data!");
+    while (1)
+    {
+        
+        if (read(fd, &buffer, 255) > 0)
+        {
+            printf("Message: %s\n", buffer);
+            memset(&buffer, 0, 255);
+        }
+    }
+
+    close(fd);
+
     return 0;
 }
